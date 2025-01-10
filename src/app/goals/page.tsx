cd2f566da8525ea3bb17ea1735fc9251e12ea875
Sam Blacklock <sam.blacklock@elasticpath.com>
2025-01-10 19:30:43 +0000

    feat: setup supabase

---
+++ src/app/goals/page.tsx
@@ -0,0 +1,17 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function Page() {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: users } = await supabase.from("users").select();
+
+  return (
+    <ul>
+      {users?.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
