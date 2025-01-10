cbb9346d1bad6fe951565d5e1af17d6718f40ce4
Sam Blacklock <sam.blacklock@elasticpath.com>
2025-01-10 19:37:16 +0000

    fix: types

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,27 +1,19 @@
 import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+export const createClient = (cookieStore: ReadonlyRequestCookies) => {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        async getAll() {
-          const cookieValues = await cookieStore;
-          return cookieValues.getAll();
-        },
-        async setAll(cookiesToSet) {
-          const cookieValues = await cookieStore;
+        getAll: () => cookieStore.getAll(),
+        setAll: (cookiesToSet) => {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieValues.set(name, value, options)
+              cookieStore.set(name, value, options)
             );
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          } catch {}
         },
       },
     }
